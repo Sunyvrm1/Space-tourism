a6d6d26cb1981b450cf45d9dc34707bd55015c99
Sunyvrm1 <verma.suny1603@gmail.com>
2023-12-01 20:50:36 +0530

    file_clean

--- main.js
+++ main.js
@@ -96,30 +96,6 @@ function opentech(techName) {
 
 //naviagtion
 
-// window.addEventListener("scroll", function () {
-//   var links = document.querySelectorAll("nav ul li");
-//   var destination1 = document.getElementById("destination");
-//   var home = document.getElementById("home");
-
-//   links.forEach(function (link) {
-//     var sectionId = link.querySelector("a").getAttribute("href").substring(1);
-//     var section = document.getElementById(sectionId);
-//     var sectionTop = section.getBoundingClientRect().top;
-//     console.log(sectionId);
-//     var viewportHeight =
-//       window.innerHeight || document.documentElement.clientHeight;
-
-//     if (sectionTop < 0.5 * viewportHeight) {
-//       links.forEach(function (link) {
-//         if (link !== link.parentNode.parentNode.querySelector(".active")) {
-//           link.classList.remove("active");
-//         }
-//       });
-//       link.classList.add("active");
-//     }
-//   });
-// });
-
 window.addEventListener("scroll", function () {
   var links = document.querySelectorAll("nav ul li");
 
